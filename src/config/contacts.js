cf669698753211129cdcedc301ec7810b123cad0
ALMAVictor <victormazoni0@gmail.com>
2026-01-15 16:37:21 -0300

    add o numero do whatsapp

--- src/config/contacts.js
+++ src/config/contacts.js
@@ -1,4 +1,4 @@
-export const WHATSAPP_NUMBER = "5534999999999"; // TODO: Update with real number
+export const WHATSAPP_NUMBER = "5534991880039";
 export const WHATSAPP_MESSAGE = "Olá! Gostaria de pedir meu botijão de gás parcelado no PIX.";
 
 export const WHATSAPP_LINK = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(WHATSAPP_MESSAGE)}`;
