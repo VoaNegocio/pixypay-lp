5e19cdf1c0580c2a6c83fce6c827d6a333224777
ALMAVictor <victormazoni0@gmail.com>
2026-01-19 12:30:22 -0300

    add att performance 1.3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
                 {/* Logo Area */}
                 <div className="flex items-center">
                     <img
-                        src="/logopixypay.png"
+                        src="/logopixypay-small.png"
                         alt="PixyPay Logo"
                         className="h-10 md:h-12 w-auto object-contain"
                     />
