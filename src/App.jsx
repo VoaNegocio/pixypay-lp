0b3fc3dcb2b6cef2173e49c53996ce1b17f47fa9
ALMAVictor <victormazoni0@gmail.com>
2026-02-11 12:33:34 -0300

    add uberlandia page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Araras from './pages/Araras';
+import Uberlandia from './pages/Uberlandia';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/uberlandia" element={<Uberlandia />} />
         <Route path="/araras" element={<Araras />} />
       </Routes>
     </Router>
